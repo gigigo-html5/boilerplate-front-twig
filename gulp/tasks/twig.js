e9ba36944252e2af1356e4d8a1bc6b297a3586b8
Francisco Jiménez <francisco.jimenez@gigigo.com>
2016-10-19 12:25:26 +0200

    Filters && Functions in Twig

--- gulp/tasks/twig.js
+++ gulp/tasks/twig.js
@@ -7,14 +7,46 @@ const ext_replace   = require('gulp-ext-replace');
 
 const config = require('../config');
 
+// Documentation: https://www.npmjs.com/package/gulp-twig
+
 gulp.task('twig', ['clean-html'], function(done) {
     return gulp.src(config.paths.src + '/twigs/*.html.twig')
-        // .pipe(data(function(file) {
-        //     return require('./../../src/data/twigData.json');
-        // }))
+
         .pipe(twig({
-            data: config.paths.twigData
+
+            cache: false,
+            data: config.paths.twigData,
+            debug: false,
+            errorLogToConsole: true,
+
+            // Custom filters
+            filters: [
+                {
+                    name: "customFilter",
+                    func: function (arg) {
+                        return "<a>" + arg + "</a>";
+                    }
+                }
+            ],
+
+            // Custom functions
+            functions: [
+                {
+                    name: "customFunction",
+                    func: function (arg) {
+                        return arg + " the custom function";
+                    }
+                },
+
+                {
+                    name: "menu",
+                    func: function () {
+                        return "<ul><li><a>Home</a></li><li><a>Options</a></li><li><a>Contact</a></li></ul>"
+                    }
+                }
+            ]
         }))
+
         .pipe(ext_replace('', '.html'))
         //.pipe(gulp.dest(config.paths.src))
         .pipe(gulp.dest(config.paths.dist))
